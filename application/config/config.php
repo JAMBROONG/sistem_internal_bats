1c4c9543b738c730968b1bddf22b71c5930d3d5c
Farhan Ma'arif Lubis <156153579+farhanlub@users.noreply.github.com>
2024-04-03 13:58:37 +0700

    p

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/sistem_internal_bats';
+$config['base_url'] = 'http://localhost/sistem_internal_bats';
 // $config['base_url'] = 'https://6447-2001-448a-20b0-e578-5099-5f0c-16d7-bba9.ngrok-free.app/sistem_internal_bats/';
 
 /*
